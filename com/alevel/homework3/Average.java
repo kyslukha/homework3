a5d7e30789a5b1a42d38cb10986d782b9d3720d6
kyslukha <kyslukha@gmail.com>
2020-12-13 01:33:50 +0200

    second commit

--- com/alevel/homework3/Average.java
+++ com/alevel/homework3/Average.java
@@ -7,16 +7,26 @@ public class Average {
         double geoAverage =1.0;
         for (int i = 0; i < 400; i++)
         {
-            myArray[i] =(int)(Math.random()*10);
+            myArray[i] =(int)(1 + Math.random()*99);
             arAverage = arAverage + myArray[i];
-            if (myArray[i]!= 0) geoAverage = geoAverage*myArray[i];
+            if (i >0) {
+
+                geoAverage = geoAverage * Math.pow(myArray[i],1/(double)i);
+                //System.out.println("geoAverage = " + geoAverage);
+                geoAverage = Math.pow(geoAverage,(double)i/(i+1));
+                //System.out.println("geoAverage = " + geoAverage);
+            }else
+            {
+                geoAverage = myArray[i];
+                //System.out.println("geoAverage = " + geoAverage);
+            }
             //System.out.println("myArray = " + myArray[i]);
-            //System.out.println("geoAverage = " + geoAverage);
+           // System.out.println("geoAverage = " + geoAverage);
         }
         arAverage = arAverage/400;
-        geoAverage = Math.abs(geoAverage);
+        //geoAverage = Math.abs(geoAverage);
         //System.out.println("geoAverage = " + geoAverage);
-        geoAverage = Math.pow(geoAverage, 0.0025);
+        //geoAverage = Math.pow(geoAverage, 0.0025);
         System.out.println("arAverage = " + arAverage);
         System.out.println("geoAverage = " + geoAverage);
 
